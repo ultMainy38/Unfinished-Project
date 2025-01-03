09d7555988cd82d7dde1a6aea467893273c9b5ca
ultMainy38 <uukkekepp@gmail.com>
2025-01-04 02:21:12 +0300

    v0.02: main menu, abt info, beginning of the game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from work_with_sprites import load_image, logo, play, info
+from work_with_sprites import load_image, logo, play, info, author, back, room, words
 import time
 
 if __name__ == "__main__":
@@ -14,8 +14,17 @@ if __name__ == "__main__":
     play_button = play(menu_sprites)
     info_button = info(menu_sprites)
 
+    info_sprites = pygame.sprite.Group()
+    word = author(info_sprites)
+    back_button = back(info_sprites)
+
+    scene_sprites = pygame.sprite.Group()
+    rooms = room(scene_sprites)
+    dialogue = words(scene_sprites)
+
+
     state = 0
-    states = ["menu", "lobby", "lvl1", "lvl2", "lvl3", "abt_info", "final"]
+    states = ["menu", "lobby", "scene", "lvl1", "lvl2", "lvl3", "abt_info", "final", "lvls"]
 
     running = True
 
@@ -24,9 +33,24 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 running = False
 
+        screen.fill((0, 0, 0))
         if states[state] == "menu":
             menu_sprites.update()
+            if info_button.update(event) == "yes":
+                state = 6
+            elif play_button.update(event) == "yes":
+                state = 2
             menu_sprites.draw(screen)
+
+        elif states[state] == "abt_info":
+            info_sprites.update()
+            if back_button.update(event) == "yes":
+                state = 0
+            info_sprites.draw(screen)
+
+        elif states[state] == "scene":
+            scene_sprites.update(event)
+            scene_sprites.draw(screen)
 
         pygame.display.flip()
         time.sleep(0.05)
--- work_with_sprites.py
+++ work_with_sprites.py
@@ -2,21 +2,16 @@ import os
 import sys
 from tokenize import group
 import random
-
 import pygame
+import time
 
 
-def load_image(name, colorkey=None):
+def load_image(name):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
     return image
 
 
@@ -35,6 +30,7 @@ class logo(pygame.sprite.Sprite):
         self.rect.x = random.randrange(290, 310)
         self.rect.y = random.randrange(90, 110)
 
+
 class play(pygame.sprite.Sprite):
     image = load_image("play_button.png")
 
@@ -46,7 +42,9 @@ class play(pygame.sprite.Sprite):
         self.rect.y = 450
 
     def update(self, *args):
-        pass
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
+            return "yes"
+
 
 class info(pygame.sprite.Sprite):
     image = load_image("add_info_button.png")
@@ -59,4 +57,62 @@ class info(pygame.sprite.Sprite):
         self.rect.y = 625
 
     def update(self, *args):
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
+            return "yes"
+
+
+class author(pygame.sprite.Sprite):
+    image = load_image("author.png")
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = author.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 300
+        self.rect.y = 100
+
+    def update(self, *args):
         pass
+
+
+class back(pygame.sprite.Sprite):
+    image = load_image("back.png")
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = back.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 10
+        self.rect.y = 600
+
+    def update(self, *args):
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
+            return "yes"
+
+
+class room(pygame.sprite.Sprite):
+    image = load_image("room.png")
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = room.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 150
+        self.rect.y = 0
+
+
+class words(pygame.sprite.Sprite):
+    image1 = load_image("dialogue1.png")
+    image2 = load_image("dialogue2.png")
+
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = words.image1
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 500
+
+    def update(self, *args):
+        if args and args[0].type == pygame.MOUSEBUTTONDOWN:
+            if self.image == words.image1:
+                self.image = words.image2
